eac7a94aac97ec6ccb2d46f71bb6db6e8b583d9d
febe19 <fabio.maddaloni@uzh.ch>
2019-10-08 10:52:55 +0200

    New Routing with Stepper
    Replaced the old routing with a new Stepper from Material UI.

--- masterprojekt_cobedias/src/Sozialanamnese/Sozialanamnese.js
+++ masterprojekt_cobedias/src/Sozialanamnese/Sozialanamnese.js
@@ -1,5 +1,5 @@
-import React, {Component} from "react";
-import {Route, Switch, NavLink} from "react-router-dom";
+import React from "react";
+import {Route} from "react-router-dom";
 import Erklaerung from "./Erklaerung";
 import Berufstaetigkeit from ".././Sozialanamnese/Berufstaetigkeit";
 import Hobbies from "../Sozialanamnese/Hobbies";
@@ -8,45 +8,121 @@ import Wohnsituation from "./Wohnsituation";
 import Zivilstand from "./Zivilstand";
 import Bemerkungen from "./Bemerkungen";
 
-class Sozialanamnese extends Component {
-    constructor(props) {
-        super(props);
-        console.log("-  " + new Date().toLocaleTimeString() + " _SozialanamneseStart_");
+import {makeStyles} from "@material-ui/core/styles";
+import Stepper from "@material-ui/core/Stepper";
+import Step from "@material-ui/core/Step";
+import StepButton from "@material-ui/core/StepButton";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        width: "100%"
+    },
+    button: {
+        marginRight: theme.spacing(1)
+    },
+    backButton: {
+        marginRight: theme.spacing(1)
+    },
+    instructions: {
+        marginTop: theme.spacing(1),
+        marginBottom: theme.spacing(1)
     }
+}));
 
+function getSteps() {
+    return ["Berufstätigkeiten", "Hobbies", "Militärdienst", "Wohnsituation", "Zivilstand", "Bemerkungen", "Abschliessen"];
+}
 
-    render() {
-        return (
-            <div>
-                <div className="Titel">
-                    <h1>Cobedias 2.0 - Sozialanamnese</h1>
-                </div>
-
-                <div className="Sozialanamnese">
-                    <ul className="header">
-                        <li><NavLink exact to="/Sozialanamnese/Berufstaetigkeit">Berufstätigkeit</NavLink></li>
-                        <li><NavLink exact to="/Sozialanamnese/Hobbies">Hobbies</NavLink></li>
-                        <li><NavLink exact to="/Sozialanamnese/Militaerdienst">Militärdienst</NavLink></li>
-                        <li><NavLink exact to="/Sozialanamnese/Wohnsituation">Wohnsituation</NavLink></li>
-                        <li><NavLink exact to="/Sozialanamnese/Zivilstand">Zivilstand</NavLink></li>
-                        <li><NavLink exact to="/Sozialanamnese/Bemerkungen">Bemerkungen</NavLink></li>
-                    </ul>
-                </div>
-                <div className="SozialanamneseContent">
-                    <Switch>
-                        <Route exact path="/Sozialanamnese" component={Erklaerung}/>
-                        <Route exact path="/Sozialanamnese/Berufstaetigkeit" component={Berufstaetigkeit}/>
-                        <Route exact path="/Sozialanamnese/Hobbies" component={Hobbies}/>
-                        <Route exact path="/Sozialanamnese/Militaerdienst" component={Militaerdienst}/>
-                        <Route exact path="/Sozialanamnese/Wohnsituation" component={Wohnsituation}/>
-                        <Route exact path="/Sozialanamnese/Zivilstand" component={Zivilstand}/>
-                        <Route exact path="/Sozialanamnese/Bemerkungen" component={Bemerkungen}/>
-                        <Route exact path="/Sozialanamnese/*" component={Erklaerung}/>
-                    </Switch>
-                </div>
-            </div>
-        );
+function getStepContent(step) {
+    switch (step) {
+        case 0:
+            return <Route component={Berufstaetigkeit}/>;
+        case 1:
+            return <Route component={Hobbies}/>;
+        case 2:
+            return <Route component={Militaerdienst}/>;
+        case 3:
+            return <Route component={Wohnsituation}/>;
+        case 4:
+            return <Route component={Zivilstand}/>;
+        case 5:
+            return <Route component={Bemerkungen}/>;
+        case 6:
+            return <Route component={Erklaerung}/>; //TODO: Hier noch den Component anpassen.
+        default:
+            return "Unknown step";
     }
 }
 
-export default Sozialanamnese;
+export default function HorizontalNonLinearAlternativeLabelStepper() {
+    const classes = useStyles();
+    const [activeStep, setActiveStep] = React.useState(0);
+    const steps = getSteps();
+
+    const handleNext = () => {
+        const newActiveStep = activeStep + 1;
+
+        setActiveStep(newActiveStep);
+    };
+
+    const handleBack = () => {
+        setActiveStep(prevActiveStep => prevActiveStep - 1);
+    };
+
+    const handleStep = step => () => {
+        setActiveStep(step);
+    };
+
+    return (
+        <div className={classes.root}>
+
+            <div className="Titel">
+                <h1>Cobedias 2.0 - Sozialanamnese</h1>
+            </div>
+
+            <Stepper alternativeLabel nonLinear activeStep={activeStep}>
+                {steps.map((label, index) => {
+                    const stepProps = {};
+                    const buttonProps = {};
+                    return (
+                        <Step key={label} {...stepProps}>
+                            <StepButton
+                                onClick={handleStep(index)}
+                                {...buttonProps}
+                            >
+                                {label}
+                            </StepButton>
+                        </Step>
+                    );
+                })}
+            </Stepper>
+            <div>
+                <div>
+                    <Typography className={classes.instructions}>
+                        {getStepContent(activeStep)}
+                    </Typography>
+                    <div>
+                        <Button
+                            disabled={activeStep === 0}
+                            onClick={handleBack}
+                            className={classes.button}
+                        >
+                            Zurück
+                        </Button>
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            onClick={handleNext}
+                            className={classes.button}
+                        >
+                            Weiter
+                        </Button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
