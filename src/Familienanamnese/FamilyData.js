ded24bdbd564a3f08cf7c2c961f65bdff06eab83
febe19 <fabio.maddaloni@uzh.ch>
2019-10-15 13:19:17 +0200

    Button to insert one Sister.
    There is now one Button working, which inserts one sister at the correct
    position.

--- src/Familienanamnese/FamilyData.js
+++ src/Familienanamnese/FamilyData.js
@@ -1,12 +1,58 @@
+//TODO: Get genderfor "me" from input form
 const myFamilyData =
     [
         {
             "id": "me",
             "gender": "male",
-            "parents": [],
+            "parents": [
+                {
+                    "id": "myMother",
+                    "type": "blood"
+                },
+                {
+                    "id": "myFather",
+                    "type": "blood"
+                }
+            ],
             "siblings": [],
             "spouses": [],
             "children": []
+        },
+        {
+            "id": "myMother",
+            "gender": "female",
+            "parents": [],
+            "siblings": [],
+            "spouses": [
+                {
+                    "id": "myFather",
+                    "type": "married"
+                }
+            ],
+            "children": [
+                {
+                    "id": "me",
+                    "type": "blood"
+                }
+            ]
+        },
+        {
+            "id": "myFather",
+            "gender": "male",
+            "parents": [],
+            "siblings": [],
+            "spouses": [
+                {
+                    "id": "myMother",
+                    "type": "married"
+                }
+            ],
+            "children": [
+                {
+                    "id": "me",
+                    "type": "blood"
+                }
+            ]
         }
     ]
 
@@ -19,75 +65,133 @@ const familyHelpers = {
 
     //Returns the number of family members
     getNumberFamilyMember: function () {
-        console.log("Return number Family member: " + myFamilyData.length);
+        console.log("__Return number Family member: " + myFamilyData.length);
         return myFamilyData.length;
     },
 
     //Get the ID of the last added family member
     getLastFamilyMember: function () {
-        console.log("Last Added Family Member: " + JSON.stringify(myFamilyData[myFamilyData.length - 1]));
+        console.log("__Last Added Family Member: " + JSON.stringify(myFamilyData[myFamilyData.length - 1]));
         return myFamilyData[myFamilyData.length - 1];
     },
 
-    //This allows adding an all new family Member
-    addFamilyMember: function (id, gender, parents, siblings, spouses, children) {
-        myFamilyData.push(
-            {
-                "id": id,
-                "gender": gender,
-                "parents": parents,
-                "siblings": siblings,
-                "spouses": spouses,
-                "children": children
+    //Get a family member by id.
+    getFamilyMemberByID: function (id) {
+        for (let i = 0; i <= myFamilyData.length - 1; i++) {
+            //If ID is the same --> found.
+            if (myFamilyData[i].id === id) {
+                return myFamilyData[i];
             }
-        );
-        console.log("Added new member to FamilyData: " + JSON.stringify(myFamilyData));
+        }
+        return false;
+    },
+
+    //This is a check if the family member already exists --> Prevention of adding the same member twice.
+    checkExistingFamilyMember: function (id) {
+        for (let i = 0; i <= myFamilyData.length - 1; i++) {
+            if (myFamilyData[i].id === id) {
+                console.log("__ Check Existing: myFamilyData[i].id: " + myFamilyData[i].id + " Already exists");
+                return true;
+            }
+        }
+        return false;
+    },
+
+    //This allows adding an all new family Member with check if they already exist
+    addFamilyMember: function (id, gender, parents, siblings, spouses, children) {
+        if (!this.checkExistingFamilyMember(id)) {
+            myFamilyData.push(
+                {
+                    "id": id,
+                    "gender": gender,
+                    "parents": parents,
+                    "siblings": siblings,
+                    "spouses": spouses,
+                    "children": children
+                }
+            );
+
+            for (let i = 0; i <= parents.length - 1; i++) {
+                let currentParent = this.getFamilyMemberByID(parents[i].id);
+                console.log("__  Current Parent: " + JSON.stringify(this.getFamilyMemberByID(parents[i].id)));
+
+                if (currentParent !== false) {
+                    currentParent.children.push(
+                        {
+                            "id" : id,
+                            "type": "blood"
+                        }
+                    )
+                    this.updateFamilyMemberByID(currentParent.id, currentParent.gender, currentParent.parents, currentParent.siblings, currentParent.spouses, currentParent.children);
+                }
+            }
+
+            for (let i = 0; i <= siblings.length - 1; i++) {
+                let currentSibling = this.getFamilyMemberByID(siblings[i].id);
+                console.log("__  Current Sibling: " + JSON.stringify(this.getFamilyMemberByID(siblings[i].id)));
+
+                if (currentSibling !== false) {
+                    currentSibling.siblings.push(
+                        {
+                            "id" : id,
+                            "type": "blood"
+                        }
+                    )
+                    this.updateFamilyMemberByID(currentSibling.id, currentSibling.gender, currentSibling.parents, currentSibling.siblings, currentSibling.spouses, currentSibling.children);
+                }
+            }
+
+            console.log("__Added new member " + id + " to FamilyData: " + JSON.stringify(myFamilyData));
+            return true;
+        } else {
+            console.log("__Family member " + id + " does already exist");
+            return false;
+        }
     },
 
     //Updating Family Members by ID
-    updateMyFamilyMember: function (id, gender, parents, siblings, spouses, children) {
-        console.log("Update of Family Member: " + id);
-
+    updateFamilyMemberByID: function (id, gender, parents, siblings, spouses, children) {
         //Iterate over Family members
-        for (var i = 0; i <= myFamilyData.length - 1; i++) {
+        for (let i = 0; i <= myFamilyData.length - 1; i++) {
 
             //If ID is the same --> found.
             if (myFamilyData[i].id === id) {
-                console.log("  Found Item at position: " + i);
 
                 //Update Gender
-                if (gender !== null) {
-                    console.log("  Update gender of " + myFamilyData[i].id + " from " + myFamilyData[i].gender + " to " + gender);
+                if (gender !== "" && myFamilyData[i].gender !== gender) {
+                    console.log("__  Update gender of " + myFamilyData[i].id + " from " + myFamilyData[i].gender + " to " + gender);
                     myFamilyData[i].gender = gender;
                 }
 
                 //Update parents
-                if (parents !== null) {
-                    console.log("  Update Parents of " + myFamilyData[i].id + " from " + JSON.stringify(myFamilyData[i].parents) + " to " + parents);
+                if (parents.length !== 0 && myFamilyData[i].parents !== parents) {
+                    console.log(parents);
+                    console.log("__  Update Parents of " + myFamilyData[i].id + " from " + JSON.stringify(myFamilyData[i].parents) + " to " + JSON.stringify(parents));
                     myFamilyData[i].parents = parents;
                 }
 
                 //Update siblings
-                if (siblings !== null) {
-                    console.log("  Update siblings of " + myFamilyData[i].id + " from " + JSON.stringify(myFamilyData[i].siblings) + " to " + siblings);
+                if (siblings.length !== 0 && myFamilyData[i].siblings !== siblings) {
+                    console.log("__  Update siblings of " + myFamilyData[i].id + " from " + JSON.stringify(myFamilyData[i].siblings) + " to " + JSON.stringify(siblings));
                     myFamilyData[i].siblings = siblings;
                 }
 
                 //Update spouse
-                if (spouses !== null) {
-                    console.log("  Update spouse of " + myFamilyData[i].id + " from " + JSON.stringify(myFamilyData[i].spouses) + " to " + spouses);
+                if (spouses.length !== 0 && myFamilyData[i].spouses !== spouses) {
+                    console.log("__  Update spouse of " + myFamilyData[i].id + " from " + JSON.stringify(myFamilyData[i].spouses) + " to " + JSON.stringify(spouses));
                     myFamilyData[i].spouses = spouses;
                 }
 
                 //Update children
-                if (children !== null) {
-                    console.log("  Update children of " + myFamilyData[i].id + " from " + JSON.stringify(myFamilyData[i].children) + " to " + children);
+                if (children.length !== 0 && myFamilyData[i].children == children) {
+                    console.log("__  Update children of " + myFamilyData[i].id + " from " + JSON.stringify(myFamilyData[i].children) + " to " + JSON.stringify(children));
                     myFamilyData[i].children = children;
                 }
+
+                //console.log("__  Update of " + myFamilyData[i].id + ": "+ JSON.stringify(myFamilyData[i]));
             }
         }
     }
-
-}
+};
 
 export default familyHelpers;
--- src/Familienanamnese/FamilyTreeComponent.js
+++ src/Familienanamnese/FamilyTreeComponent.js
@@ -1,11 +1,10 @@
-import React, {useState} from 'react';
+import React, {Component, useState} from 'react';
 import {IFamilyExtNode} from 'relatives-tree';
 import ReactFamilyTree from 'react-family-tree';
 import FamilyNode from './FamilyNode';
 import styles from './FamilyTree.module.css';
 import Button from "@material-ui/core/Button";
 
-import localStorage from "local-storage";
 import familyHelpers from "./FamilyData.js";
 
 //My ID is always 0.
@@ -14,52 +13,99 @@ const myID = 'me';
 const WIDTH = 100;
 const HEIGHT = 100;
 
-function FamilyTree() {
-    const [rootId] = useState(myID);
+function familyTreeWorker() {
+
+    const meAsSibling = [
+        {
+            "id": "me",
+            "type": "blood"
+        }
+    ]
+
+    familyHelpers.updateFamilyMemberByID = familyHelpers.updateFamilyMemberByID.bind(this);
 
     //Write Data to Local Storage
-    localStorage.set('myFamilyData', familyHelpers.getFamilyData());
+    //localStorage.set('myFamilyData', familyHelpers.getFamilyData());
 
     //Get number of Family Members
     familyHelpers.getNumberFamilyMember();
 
-    //Adding of other family members
-    familyHelpers.addFamilyMember("mother", "female", [], [], [],[]);
+    //Adding of other family members with check if they already exist.
+//    familyHelpers.addFamilyMember("myMother", "female", [], [], [],[]);
 
     //Get ID of the last family member
-    let data = familyHelpers.getLastFamilyMember();
-    console.log("ID of last added Family Member: " + data.id);
-    familyHelpers.updateMyFamilyMember("me", "female", [], [], [], []);
-    console.log("Family Data " + JSON.stringify(familyHelpers.getFamilyData()));
+//    let data = familyHelpers.getLastFamilyMember();
+//    console.log("ID of last added Family Member: " + data.id);
+
+    //Get a Family Member by its ID.
+//    console.log("Get FamilyMemberByID: "+ JSON.stringify(familyHelpers.getFamilyMemberByID("myMother")));
+
+    //Update a Family Member
+    let me = familyHelpers.getFamilyMemberByID("me");
+    //familyHelpers.updateFamilyMemberByID("me", "female", [], me.siblings, me.spouses, me.children);
+    //console.log("Family Data " + JSON.stringify(familyHelpers.getFamilyData()));
+
+    //add sister1 for example
+    //familyHelpers.addFamilyMember("sister1", "female", me.parents, me.siblings, [], []);
+    familyHelpers.addFamilyMember("brother1", "male", me.parents, me.siblings, [], []);
+
+    familyHelpers.getLastFamilyMember();
 
 
-    localStorage.set('myFamilyData', familyHelpers.getFamilyData());
+    //localStorage.set('myFamilyData', familyHelpers.getFamilyData());
 
-    return (
-        <div>
+}
+
+
+class FamilyTree extends Component {
+
+    constructor(props) {
+        super(props);
+
+        //Define the state of this component.
+        this.state = {
+            FamilyDataState: familyHelpers.getFamilyData()
+        };
+    }
+
+
+    addSister = () => {
+        let me = familyHelpers.getFamilyMemberByID("me");
+        familyHelpers.addFamilyMember("sister1", "female", me.parents, me.siblings, [], []);
+        this.setState(
+            {FamilyDataState: familyHelpers.getFamilyData()}
+        )
+    }
+
+
+    render() {
+        return (
             <div>
-                <ReactFamilyTree
-                    nodes={familyHelpers.getFamilyData()}
-                    rootId={rootId}
-                    width={WIDTH}
-                    height={HEIGHT}
-                    canvasClassName={styles.tree}
-                    renderNode={(node: IFamilyExtNode) => (
-                        <FamilyNode
-                            key={node.id}
-                            node={node}
-                            isRoot={node.id === rootId}
-                            style={{
-                                width: WIDTH * 0.8,
-                                height: HEIGHT * 0.8,
-                                transform: `translate(${node.left * (WIDTH / 2)}px, ${node.top * (HEIGHT / 2)}px)`,
-                            }}
-                        />
-                    )}
-                />
+                <div>
+                    <ReactFamilyTree
+                        nodes={this.state.FamilyDataState}
+                        rootId={myID}
+                        width={WIDTH}
+                        height={HEIGHT}
+                        canvasClassName={styles.tree}
+                        renderNode={(node: IFamilyExtNode) => (
+                            <FamilyNode
+                                key={node.id}
+                                node={node}
+                                isRoot={node.id === myID}
+                                style={{
+                                    width: WIDTH * 0.8,
+                                    height: HEIGHT * 0.8,
+                                    transform: `translate(${node.left * (WIDTH / 2)}px, ${node.top * (HEIGHT / 2)}px)`,
+                                }}
+                            />
+                        )}
+                    />
+                </div>
+                <Button onClick={this.addSister}>Add Sister</Button>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default FamilyTree;
