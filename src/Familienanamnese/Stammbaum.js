07262905744a7968a2b1ec617418ea3f18f9b3b9
Indy <ivo.indergand@uzh.ch>
2019-10-18 14:34:53 +0200

    'First_step_tutorial'

--- src/Familienanamnese/Stammbaum.js
+++ src/Familienanamnese/Stammbaum.js
@@ -1,101 +1,111 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import Box from "@material-ui/core/Box";
-import FamilyTree from './FamilyTreeComponent'
-import styles from './FamilyTree.module.css';
+import FamilyTree from "./FamilyTreeComponent";
+import styles from "./FamilyTree.module.css";
 
 export class Stammbaum extends Component {
-    continue = e => {
-        e.preventDefault();
-        this.props.nextStep();
-    };
+  constructor(props) {
+    super(props);
+  }
 
-    back = e => {
-        e.preventDefault();
-        this.props.prevStep();
-    };
+  state = {
+    color: "white"
+  };
 
-    render() {
-        return (
-            <div>
-                <div style={{width: "100%"}}>
-                    <Box
-                        display="flex"
-                        justifyContent="flex-start"
-                        m={1}
-                        p={1}
-                        bgcolor="background.paper"
-                    >
-                        <Box p={1}>
-                            <img
-                                src={require("../components/images/logo_cobedias.png")}
-                                style={{width: "40%"}}
-                                m={1}
-                                p={1}
-                            />
-                        </Box>
-                    </Box>
+  continue = e => {
+    e.preventDefault();
+    this.props.nextStep();
+  };
 
-                    <div className={styles.root}>
-                        <FamilyTree/>
-                    </div>
+  back = e => {
+    e.preventDefault();
+    this.props.prevStep();
+  };
 
-                    <Box
-                        display="flex"
-                        justifyContent="flex-end"
-                        flexDirection="column"
-                        alignItems="flex-end"
-                        m={1}
-                        p={1}
-                        bgcolor="background.paper"
-                    >
-                        <Box p={1}>
-                            <img
-                                src={require("../components/images/weiblich_hinz.png")}
-                                style={{width: "40%"}}
-                                m={1}
-                                p={1}
-                            />
-                            <img
-                                src={require("../components/images/mänlich_hinz.png")}
-                                style={{width: "45%"}}
-                                m={1}
-                                p={1}
-                            />
-                        </Box>
-                        <Box p={1}>
-                            <img
-                                src={require("../components/images/weiblich_hinz.png")}
-                                style={{width: "40%"}}
-                                m={1}
-                                p={1}
-                            />
-                            <img
-                                src={require("../components/images/mänlich_hinz.png")}
-                                style={{width: "45%"}}
-                                m={1}
-                                p={1}
-                            />
-                        </Box>
-                        <Box p={1}>
-                            <img
-                                src={require("../components/images/andere.png")}
-                                title="Halb"
-                                style={{width: "30%"}}
-                                m={2}
-                                p={2}
-                            />
-                            <img
-                                src={require("../components/images/andere.png")}
-                                style={{width: "30%"}}
-                                m={2}
-                                p={2}
-                            />
-                        </Box>
-                    </Box>
-                </div>
-            </div>
-        );
-    }
+  changeimgcolor = e => {
+    this.setState({
+      color: "rgba(105,105,105,0.5)"
+    });
+  };
+
+  render() {
+    return (
+      <div style={{ backgroundColor: this.state.color, width: "100%" }}>
+        <Box display="flex" justifyContent="flex-start" m={1} p={1}>
+          <Box p={1}>
+            <img
+              src={require("../components/images/logo_cobedias.png")}
+              style={{ width: "40%" }}
+              m={1}
+              p={1}
+            />
+          </Box>
+        </Box>
+
+        <FamilyTree />
+
+        <Box
+          display="flex"
+          justifyContent="flex-end"
+          flexDirection="column"
+          alignItems="flex-end"
+          m={1}
+          p={1}
+        >
+          <Box p={1}>
+            <img
+              src={require("../components/images/weiblich_hinz.png")}
+              style={{ width: "40%" }}
+              m={1}
+              p={1}
+            />
+            <img
+              src={require("../components/images/mänlich_hinz.png")}
+              style={{ width: "45%" }}
+              m={1}
+              p={1}
+            />
+          </Box>
+          <Box p={1}>
+            <img
+              src={require("../components/images/weiblich_hinz.png")}
+              style={{ width: "40%" }}
+              m={1}
+              p={1}
+            />
+            <img
+              src={require("../components/images/mänlich_hinz.png")}
+              style={{ width: "45%" }}
+              m={1}
+              p={1}
+            />
+          </Box>
+          <Box p={1}>
+            <img
+              src={require("../components/images/andere.png")}
+              title="Halb"
+              style={{ width: "30%" }}
+              m={2}
+              p={2}
+            />
+            <img
+              src={require("../components/images/andere.png")}
+              style={{ width: "30%" }}
+              m={2}
+              p={2}
+            />
+
+            <img
+              src={require("../components/images/question.png")}
+              m={2}
+              p={2}
+              onClick={this.changeimgcolor}
+            />
+          </Box>
+        </Box>
+      </div>
+    );
+  }
 }
 
 export default Stammbaum;
