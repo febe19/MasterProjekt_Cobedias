d113dc04b02e6ea032e875069cff0e905a85468e
AnnaGotz <anna_goetz@gmx.at>
2019-11-21 23:15:13 +0100

    text changes on intro screen

--- src/Intro.js
+++ src/Intro.js
@@ -20,23 +20,28 @@ export class Intro extends Component {
                     <div className="ChangingContent">
                         <h1>Herzlich Willkommen</h1>
                         <br/>
-                        <p>Vielen Dank, dass Sie sich Zeit nehmen heute unseren Prototyp zu testen.</p>
+                        <p>Vielen Dank, dass Sie sich Zeit nehmen, heute unseren Prototyp zu testen.</p>
                         <br/>
                         <h3>Generell</h3>
-                        <p>In diesem Prototyp sollten Sie Angaben zu Ihnen sowie Ihrere Familie machen. Diese zwei
-                            Untersuche heissen "Sozialanamnese" sowie "Familienanamnese" und sind Teil der Cobedias
-                            Untersuchung von Prof. Dr. Barbara Biedermann.</p>
+                        <p>In diesem Prototyp sollten Sie Angaben zu Ihnen sowie Ihrer Familie (zur Berechnung des
+                            eigenen Krankheitsrisikos) machen. Diese Aufgaben sind in zwei Teile unterteilt -
+                            "Sozialanamnese" und "Familienanamnese". Sie sind Teil der regulären Cobedias-Untersuchung
+                            von Prof. Dr. Barbara Biedermann.</p>
                         <br/>
                         <h3>Einige wichtige Hinweise:</h3>
                         <ul>
-                            <li>Bitte beide Untersuche ausfüllen.</li>
-                            <li>Daten bitte möglichst wahrheitsgetreu angeben.</li>
-                            <li>Wenn beide Anamnesen abgeschlossen werden, wird je ein PDF-Dokument heruntergeladen.</li>
+                            <li>Bitte füllen Sie beide Teile (Sozialanamnese und Familienanamnese) aus.</li>
+                            <li>Bitte geben Sie möglichst wahrheitsgetreue und vollständige Daten an. Sie können
+                            jedoch Fantasienamen oder nur die Initialen Ihrer Familienmitglieder verwenden, um deren
+                            Identitäten zu schützen. Alle Daten werden jedoch selbstverständlich vertraulich
+                            behandelt.</li>
+                            <li>Pro abgeschlossener Anamnese wird je ein PDF-Dokument heruntergeladen. Dieses können
+                            Sie einfach an der vorgeschlagenen Stelle speichern.</li>
                         </ul>
                     </div>
                     <div className="StartButtonDiv">
                         <NavLink exact to="/Home" style={{"text-decoration": "none"}}>
-                            <Button variant="contained" color="primary">Alles Klar. Los Gehts!</Button>
+                            <Button variant="contained" color="primary">Alles klar. Los gehts!</Button>
                         </NavLink>
                     </div>
                 </div>
